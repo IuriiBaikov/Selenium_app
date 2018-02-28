a7597ec86152063a3d6315588232e56d3e3cf84b
medvedyc <medvedyc@hotmail.com>
2018-02-28 09:30:38 +0300

    Template for test Selenium settings

--- Selenium_app/Program.cs
+++ Selenium_app/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using OpenQA.Selenium;
 
 namespace Selenium_app
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //System.setProperty("webdriver.chrome.driver", "/usr/local/bin/ChromeDriver/chromedriver");
+            IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+           // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Window.Maximize();
+
+         //   driver.Manage().Timeouts().ImplicitWait.Milliseconds.(10, TimeUnit.SECONDS);
+            driver.Url = "http://youtube.com";
+
+            driver.FindElement(By.CssSelector("#search")).SendKeys("Денчик грудные");
+            driver.FindElement(By.CssSelector("#search-icon-legacy > yt-icon")).Click();
+            Thread.Sleep(10000);
+            driver.Close();
         }
     }
 }
